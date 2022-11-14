af4a228b25363fb34de0e6bea41c255940ed96c9
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2022-11-14 23:52:02 +0700

    Optimasi image loading dengan WebPack

--- Web/src/script/components/team-item.js
+++ Web/src/script/components/team-item.js
@@ -8,7 +8,7 @@ class TeamItem extends HTMLElement {
         this.innerHTML = 
         `<div class="col-lg-3 col-md-3 col-sm-6 my-4 mx-auto">
         <div class="card text-center border-0 bg-transparent">
-            <img class=" card-img-top mb-3" src="./assets/images/${this._team.photo}" alt="">
+            <img class=" card-img-top mb-3" src=${this._team.photo} alt="">
             <h5 class="card-title osw">${this._team.name}</h5>
             <p class="card-text ppns">${this._team.id}</p>
         </div>
--- Web/src/script/main.js
+++ Web/src/script/main.js
@@ -1,25 +1,35 @@
 import './components/shop-grid.js';
 import './components/corousel.js';
 import './components/team-list';
+import githubImg from '../assets/images/GitHub-logo.png';
 
 const main = () => {
-  
-  
-  // formerly in navbar.js
-  document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 0) {
-        document.getElementById("navbar_top").classList.add("fixed-top");
-        // add padding top to show content behind navbar
-        const navbar_height = document.querySelector(".navbar").offsetHeight;
-        document.body.style.paddingTop = navbar_height + "px";
-      } else {
-        document.getElementById("navbar_top").classList.remove("fixed-top");
-        // remove padding top from body
-        document.body.style.paddingTop = "0";
-      }
-    });
+  setNavbar();
+  setFooter();
+}
+
+// formerly navbar.js
+const setNavbar = () => {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      const navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+    }
   });
+}
+
+const setFooter = () => {
+  const footer = document.getElementById('foot');
+  
+  const githubImageElement = new Image();
+  githubImageElement.src = githubImg;
+  footer.appendChild(githubImageElement);
 }
 
 export default main;
