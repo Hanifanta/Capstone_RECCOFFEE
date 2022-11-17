a214c4c0648d26af22cee90d9d8fca46bf3272d9
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2022-11-17 23:54:34 +0700

    Menambahkan integrasi dengan RapidAPI

--- Web/src/script/components/shop-grid.js
+++ Web/src/script/components/shop-grid.js
@@ -1,13 +1,19 @@
 import "./shop-item.js";
 
 class ShopGrid extends HTMLElement {
-  connectedCallback() {
+  // connectedCallback() {
+  //   this.render();
+  // }
+
+  set products(products){
+    this._products = products;
     this.render();
   }
 
   render() {
     for (let i = 0; i < 10; i++) {
       const shopItem = document.createElement("shop-item");
+      shopItem.product = this._products[i];
       this.appendChild(shopItem);
     }
   }
--- Web/src/script/components/shop-item.js
+++ Web/src/script/components/shop-item.js
@@ -1,7 +1,12 @@
 import $ from "jquery";
 
 class ShopItem extends HTMLElement {
-    connectedCallback() {
+    // connectedCallback() {
+    //     this.render();
+    // }
+
+    set product(product){
+        this._product = product;
         this.render();
     }
 
@@ -10,7 +15,11 @@ class ShopItem extends HTMLElement {
         
         this.innerHTML = 
         `<div class="card shadow border-0 rounded-3">
-            
+            <img class="card-img-top mb-2" src=${this._product.product_photos[0]}> 
+            <div class="card-body">
+                <span class="card-title osw">${this._product.product_title}</span>
+                <span class="card-subtitle ppns">${this._product.offer.price}</span>   
+            </div>  
         </div>`;
     }
 }
