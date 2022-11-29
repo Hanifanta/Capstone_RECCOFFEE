901db56981da3b9b83e341939496a6481e64cf08
Hamza Firdaus <hamzaf625@gmail.com>
2022-11-29 10:53:59 +0700

    Penambahan focus navbar, api call, dan responsive.

--- Web/src/script/components/corousel-list.js
+++ Web/src/script/components/corousel-list.js
@@ -12,11 +12,36 @@ class CorouselList extends HTMLElement {
 
   render() {
     this.innerHTML = `
+    <style>
+    .glide__arrow {
+      border-radius : 50%;
+      padding:calc(10px + 0.1vw);
+      background : rgba(255, 255, 255, 0.5);
+    }
+    
+    @media screen and (max-width: 576px) {
+      .glide__arrow {
+        top: 36vh;
+      }
+    }
+    </style>
     <div class="corousel d-flex align-items-center justify-content-center">
           <div class="glide">
             <div class="glide__track" data-glide-el="track">
               <ul class="glide__slides">
               </ul>
+            </div>
+            <div class="glide__arrows" data-glide-el="controls">
+              <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                <path d="M0 12l10.975 11 2.848-2.828-6.176-6.176H24v-3.992H7.646l6.176-6.176L10.975 1 0 12z"></path>
+              </svg>
+            </button>
+              <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+                <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"></path>
+              </svg>
+            </button>
             </div>
           </div>
         </div>
@@ -24,7 +49,7 @@ class CorouselList extends HTMLElement {
 
     const baseURLhot = "https://api.sampleapis.com/coffee/hot";
     const baseURLiced = "https://api.sampleapis.com/coffee/iced";
-    Promise.all([fetch(baseURLiced), fetch(baseURLhot)])
+    Promise.all([fetch(baseURLhot), fetch(baseURLiced)])
       .then((responses) => {
         return Promise.all(
           responses.map((response) => {
@@ -47,8 +72,7 @@ class CorouselList extends HTMLElement {
       const seen = new Set();
       const filteredArr = data.filter((item) => {
         const duplicate = seen.has(item.description);
-        const imageurl = item.image.includes("https");
-        if (imageurl) {
+        if (item.image == null ? false : item.image.includes("https")) {
           seen.add(item.description);
           return !duplicate;
         }
@@ -62,7 +86,7 @@ class CorouselList extends HTMLElement {
 
       new Glide(".glide", {
         type: "slider",
-        autoplay: 3000,
+        hoverpause: false,
         perView: 3,
         bound: true,
         breakpoints: {
--- Web/src/script/components/hasil-rekomendasi.js
+++ Web/src/script/components/hasil-rekomendasi.js
@@ -1,12 +1,13 @@
 import $ from "jquery";
 class Hasil extends HTMLElement {
   set hasilnya(item) {
+    console.log(item);
     this._item = item;
     this.render();
   }
 
   render() {
-    this.classList.add("hasil-rekomendasi", "p-5", "m-auto", "d-grid");
+    this.classList.add("hasil-rekomendasi", "m-auto", "d-grid");
     this.innerHTML = `
        
               <div class="row">
@@ -15,8 +16,8 @@ class Hasil extends HTMLElement {
                   <h1 class="mb-auto">${this._item.rating}</h1>
                 </div>
                 <div class="col-8 col-lg-10 d-grid">
-                  <h1 class="m-0 mt-auto">${this._item.name}</h1>
-                  <p class="m-0 mb-auto">${this._item.origin}</p>
+                  <h1 class="m-0 mt-auto">${this._item.roaster}</h1>
+                  <p class="m-0 mb-auto">${this._item.name}</p>
                 </div>
               </div>
               <div class="row my-3">
@@ -42,14 +43,39 @@ class Hasil extends HTMLElement {
                     </p>
                   </div>
                 </div>
+                <div class="d-flex">
+                  <button
+                    id="button-other-coffee"
+                    class="btn btn-sm btn-primary text-white fw-bold me-3"
+                  >
+                    See other coffee
+                  </button>
+
+                  <a
+                    id="button-other-coffee"
+                    class="btn btn-sm btn-primary text-white fw-bold me-auto"
+                    target="_blank" 
+                    href="https://www.google.com/search?tbm=shop&q=${this._item.name}+coffee">
+                    Search on Google
+                  </a>
+
+                  <div class="ms-auto">
+                  <a href="${this._item.link}" 
+                  target="_blank" 
+                  class="me-auto">
+                  <i>Details ></i>
+                  </a>
+                  </div>
+
+                </div>
               </div>
-              <button id="button-other-coffee" class="btn btn-sm btn-primary text-white fw-bold me-auto">
-                See other coffee
-              </button>
+            
+              
           
     `;
     const show = () => {
       $("#recommendation").toggleClass("d-none");
+      $("#recommendation")[0].scrollIntoView();
     };
     this.querySelector("#button-other-coffee").addEventListener("click", show);
   }
